f5235999b19aea2b11d0f137b1cc81a0a9180f74
folecr <folecr@gmail.com>
2012-10-16 13:26:35 -0700

    Has conversions to standard types only.
     * Cocos specific conversions are in a separate file.

--- targets/spidermonkey/common/js_manual_conversions.h
+++ targets/spidermonkey/common/js_manual_conversions.h
@@ -3,6 +3,9 @@
 //  Copyright (c) 2012 Zynga Inc. All rights reserved.
 //
 
+#ifndef __JS_MANUAL_CONVERSIONS_H__
+#define __JS_MANUAL_CONVERSIONS_H__
+
 #include "chipmunk.h"
 #include "cocos2d.h"
 
@@ -14,24 +17,14 @@ JSBool jsval_to_opaque( JSContext *cx, jsval vp, void **out );
 JSBool jsval_to_int( JSContext *cx, jsval vp, int *out);
 JSBool jsval_to_long( JSContext *cx, jsval vp, long *out);
 JSBool jsval_to_longlong( JSContext *cx, jsval vp, long long *out);	
-	
-JSBool jsval_to_CGPoint( JSContext *cx, jsval vp, cpVect *out );
-
-jsval CGPoint_to_jsval( JSContext *cx, cpVect p );
-JSBool jsval_to_CCPoint( JSContext *cx, jsval vp, CCPoint *ret );
 
 jsval int_to_jsval( JSContext *cx, int l);
 jsval long_to_jsval( JSContext *cx, long l);
 jsval longlong_to_jsval( JSContext *cx, long long l);
 jsval opaque_to_jsval( JSContext *cx, void* opaque);
-	
-	
-    JSBool jsval_to_cpBB( JSContext *cx, jsval vp, cpBB *ret );
-    jsval cpBB_to_jsval(JSContext *cx, cpBB bb );
     
 #ifdef __cplusplus
 }
 #endif
 
-#define cpVect_to_jsval CGPoint_to_jsval
-#define jsval_to_cpVect jsval_to_CGPoint
+#endif /* __JS_MANUAL_CONVERSIONS_H__ */
