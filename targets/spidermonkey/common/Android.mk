8bd7fdf6bfb9774dd5bc7be8821c82abd1751b86
folecr <folecr@gmail.com>
2012-10-16 13:37:18 -0700

    * Remove cocos references in makefile  * Remove use of flag -DCOCOS2D_JAVASCRIPT  * Remove import-module of cocos2dx

--- targets/spidermonkey/common/Android.mk
+++ targets/spidermonkey/common/Android.mk
@@ -6,19 +6,13 @@ LOCAL_MODULE := scriptingcore-spidermonkey
 
 LOCAL_MODULE_FILENAME := libscriptingcore-spidermonkey
 
-LOCAL_SRC_FILES := ScriptingCore.cpp \
-                   cocos2d_specifics.cpp
-
-LOCAL_CFLAGS := -DCOCOS2D_JAVASCRIPT
-
-LOCAL_EXPORT_CFLAGS := -DCOCOS2D_JAVASCRIPT
+LOCAL_SRC_FILES := ScriptingCore.cpp
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)
 
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
 
 LOCAL_WHOLE_STATIC_LIBRARIES := spidermonkey_static
-LOCAL_WHOLE_STATIC_LIBRARIES += cocos2dx_static
 
 LOCAL_LDLIBS := -landroid
 LOCAL_LDLIBS += -llog
@@ -26,4 +20,3 @@ LOCAL_LDLIBS += -llog
 include $(BUILD_STATIC_LIBRARY)
 
 $(call import-module,spidermonkey/android)
-$(call import-module,cocos2dx)
