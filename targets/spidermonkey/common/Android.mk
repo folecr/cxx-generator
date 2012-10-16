52c6e320bdf2d925dc4628198a2d8e2f24747ad2
folecr <folecr@gmail.com>
2012-10-16 13:37:18 -0700

    Remove cocos references in makefile

--- targets/spidermonkey/common/Android.mk
+++ targets/spidermonkey/common/Android.mk
@@ -6,8 +6,7 @@ LOCAL_MODULE := scriptingcore-spidermonkey
 
 LOCAL_MODULE_FILENAME := libscriptingcore-spidermonkey
 
-LOCAL_SRC_FILES := ScriptingCore.cpp \
-                   cocos2d_specifics.cpp
+LOCAL_SRC_FILES := ScriptingCore.cpp
 
 LOCAL_CFLAGS := -DCOCOS2D_JAVASCRIPT
 
@@ -18,7 +17,6 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
 
 LOCAL_WHOLE_STATIC_LIBRARIES := spidermonkey_static
-LOCAL_WHOLE_STATIC_LIBRARIES += cocos2dx_static
 
 LOCAL_LDLIBS := -landroid
 LOCAL_LDLIBS += -llog
