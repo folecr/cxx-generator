c356b184ddf8e296334a9d9c0eef03fc8b6aa407
folecr <folecr@gmail.com>
2012-10-18 23:13:55 -0700

    Android makefile
     * Add reference to cocos2dx (until all usage of cocos2dx api's are cleaned up)

--- targets/spidermonkey/common/Android.mk
+++ targets/spidermonkey/common/Android.mk
@@ -2,21 +2,24 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := scriptingcore-spidermonkey
+LOCAL_MODULE := cxxgenerator-spidermonkey
 
-LOCAL_MODULE_FILENAME := libscriptingcore-spidermonkey
+LOCAL_MODULE_FILENAME := libcxxgenerator-spidermonkey
 
-LOCAL_SRC_FILES := jsbScriptingCore.cpp
+LOCAL_SRC_FILES := jsbScriptingCore.cpp \
+                   js_manual_conversions.cpp \
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)
 
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
 
 LOCAL_WHOLE_STATIC_LIBRARIES := spidermonkey_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocos2dx_static
 
 LOCAL_LDLIBS := -landroid
 LOCAL_LDLIBS += -llog
 
 include $(BUILD_STATIC_LIBRARY)
 
-$(call import-module,spidermonkey/android)
+$(call import-module,scripting/javascript/spidermonkey-android)
+$(call import-module,cocos2dx)
