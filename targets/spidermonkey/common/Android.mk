eb360635bbda5a8ff378c3331d22ee2fdb075071
folecr <folecr@gmail.com>
2012-10-21 10:53:08 -0700

    Add reference to cocos2dx (until all usage of cocos2dx api's are cleaned up)

--- targets/spidermonkey/common/Android.mk
+++ targets/spidermonkey/common/Android.mk
@@ -14,6 +14,7 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
 
 LOCAL_WHOLE_STATIC_LIBRARIES := spidermonkey_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocos2dx_static
 
 LOCAL_LDLIBS := -landroid
 LOCAL_LDLIBS += -llog
@@ -21,3 +22,4 @@ LOCAL_LDLIBS += -llog
 include $(BUILD_STATIC_LIBRARY)
 
 $(call import-module,scripting/javascript/spidermonkey-android)
+$(call import-module,cocos2dx)
